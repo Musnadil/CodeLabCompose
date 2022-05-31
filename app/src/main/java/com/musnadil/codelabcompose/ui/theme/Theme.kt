2f3052684211b6b601502b15a4c1f6e72b918feb
Musnadil <firdaus.musnadil@gmail.com>
2022-05-31 19:06:23 +0700

    Initial commit

---
+++ app/src/main/java/com/musnadil/codelabcompose/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.musnadil.codelabcompose.ui.theme
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface= Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+@Composable
+fun CodeLabComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
